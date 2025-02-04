728a2c951219c2fd3be318029c936a0a83bac96a
Ananias Raphael <ananias.nobrega@solutis.com.br>
2025-02-04 01:37:23 -0300

    :wrench: postgres config to acess container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataBaseConfigService } from './config/db.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DataBaseConfigService,
+      inject: [DataBaseConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
