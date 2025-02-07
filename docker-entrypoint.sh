6386cbf178ecc6668df646346fa8eca344432edb
Ananias Raphael <ananias.nobrega@solutis.com.br>
2025-02-07 05:23:14 -0300

    :bug: fix docker and migrations fixed

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for postgres..."
+until nc -z postgres 5432; do
+  sleep 1
+done
+echo "PostgreSQL started"
+
+echo "Running migrations..."
+npm run migration:run
+
+echo "Starting application..."
+npm run start:prod
